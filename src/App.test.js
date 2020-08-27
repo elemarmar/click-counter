2ea2f8b73a2454719d89cf58f14ea4b1b4113421
Elena Martínez Marín <elemartinezmarin@protonmail.com>
2020-08-27 12:21:56 +0200

    Start project, add first tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import Enzyme, { shallow } from 'enzyme';
+import EnzymeAdapter from 'enzyme-adapter-react-16';
 import App from './App';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+Enzyme.configure( { adapter: new EnzymeAdapter });
+
+test('renders without any error', () => {
+	const wrapper = shallow(<App />);
+    const appComponent = wrapper.find("[data-test='component-app']");
+    expect(appComponent.length).toBe(1);
+})
+
+test('renders increment button', () => {
+	const wrapper = shallow(<App />);
+	const incrementButton = wrapper.find("[data-test='increment-button']");
+	expect(incrementButton.length).toBe(1);
+})
+
+test('renders counter display', () => {
+	const wrapper = shallow(<App />);
+	const counterDisplay = wrapper.find("[data-test='counter-display']");
+	expect(counterDisplay.length).toBe(1);
+})
+
+test('counter starts at 0', () => {
+  // code will go here
+})
+
+test('clicking button increments counter', () => {
+  // code will go here
+})
